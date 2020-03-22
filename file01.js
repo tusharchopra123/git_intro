a0fce31a2c569ebbd644eb3f92da7dec97fe71a3
tusharchopra123 <tusharchopra123@gmail.com>
2020-03-22 18:34:14 +0530

    add 'nice to have 'msg

--- file01.js
+++ file01.js
@@ -2,6 +2,7 @@ function greet(name){
     console.log("knock knock knock "+name)
     console.log("knock knock knock "+name)
     console.log("knock knock knock "+name)
+    console.log("NIce to have you here")
 
 }
 
